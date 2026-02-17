984d3f4883626784775dc234b6a572ca980e8860
1 прога <ravil@gmail.com>
2026-02-17 12:32:54 +0500

    странный этот ваш сканер и принт жаловался мне что нельзя одновременно слово и что то вывести, пришлось делать 2 принта для того и того

--- src/proga.java
+++ src/proga.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 void main() {
     System.out.println("Popa");
     System.out.println(3e3); /* короче тут 3 эт число e это степень 1,10,100,1000 и т.д. а 3 в данном случае значит 1000
                                 если поменять на 2 то будет возведение в 100, а вывод 300, так же можно указать отрицательное
                                 число и число будет дробным? короче после ноля тип 0.3 */
     System.out.println(3.2e4);
+
+    Scanner in = new Scanner(System.in);
+    System.out.print("Введите число: ");
+    int num = in.nextInt();
+
+    System.out.print("Число: ");
+    System.out.println(num);
+    in.close();
 }
